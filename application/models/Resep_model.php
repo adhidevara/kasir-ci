d67b70e955e75e1397508f34dcd9815127d7f47a
antoniusandre02 <75978607+antoniusandre02@users.noreply.github.com>
2024-02-19 23:23:25 +0700

    update getDetailResep di Resep_model
    menambahkan query getDetailResep

--- application/models/Resep_model.php
+++ application/models/Resep_model.php
@@ -47,6 +47,11 @@ class Resep_model extends CI_Model {
         $this->db->from('bahan');
 		return $this->db->get();
     }
+
+	function getDetailResep(){
+		$this->db->query('SELECT dr.id AS Id_Detail_Resep,b.nama AS Nama_Bahan, r.nama AS Nama_Resep, sp.satuan AS Satuan, b.unit_cost AS Harga_per_Item, dr.qty AS Proporsi_Bahan_Baku, r.cost_resep AS HPP, dr.bahan_utama AS is_Bahan_Utama FROM satuan_produk sp RIGHT JOIN bahan b ON sp.id = b.id JOIN detail_resep dr ON b.id = dr.id_bahan  JOIN resep r ON dr.id_resep = r.id');
+		return $this->db->get()->result();
+	}
 }
 
 /* End of file Bahan_model.php */
